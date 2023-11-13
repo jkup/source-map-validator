2169cb99b5b455c93a9af94ac578f33b33c1ac67
Jon Kuperman <jon.kuperman@gmail.com>
2023-11-13 17:01:20 +0000

    wrap in async function

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import yargs from "yargs";
-import fs from "fs";
 import path from "path";
 
 import { validateSourceMapJSON } from "./validators/validateSourceMapJSON.mjs";
@@ -9,33 +8,37 @@ import { validateSourceMapFormat } from "./validators/validateSourceMapFormat.mj
 import { validateSourceFiles } from "./validators/validateSourceFiles.mjs";
 import { validateSourceMapMappings } from "./validators/validateSourceMapMappings.mjs";
 
-const options = yargs(process.argv.slice(2))
-  .usage("Usage: -s <source map> -g <generated file> -o <original file>")
-  .option("s", {
-    alias: "sourceMap",
-    describe: "Source map file path",
-    type: "string",
-    demandOption: true,
-  })
-  .option("g", {
-    alias: "generatedFile",
-    describe: "Generated file path",
-    type: "string",
-    demandOption: true,
-  })
-  .option("o", {
-    alias: "originalFolder",
-    describe: "Original folder path",
-    type: "string",
-    demandOption: true,
-  }).argv;
+async function validateSourceMap() {
+  const options = yargs(process.argv.slice(2))
+    .usage("Usage: -s <source map> -g <generated file> -o <original file>")
+    .option("s", {
+      alias: "sourceMap",
+      describe: "Source map file path",
+      type: "string",
+      demandOption: true,
+    })
+    .option("g", {
+      alias: "generatedFile",
+      describe: "Generated file path",
+      type: "string",
+      demandOption: true,
+    })
+    .option("o", {
+      alias: "originalFolder",
+      describe: "Original folder path",
+      type: "string",
+      demandOption: true,
+    }).argv;
 
-const sourceMapPath = path.resolve(options.sourceMap);
-const originalFolderPath = path.resolve(options.originalFolder);
-const generatedFilePath = path.resolve(options.generatedFile);
+  const sourceMapPath = path.resolve(options.sourceMap);
+  const originalFolderPath = path.resolve(options.originalFolder);
+  const generatedFilePath = path.resolve(options.generatedFile);
 
-const sourceMap = validateSourceMapJSON(sourceMapPath);
+  const sourceMap = validateSourceMapJSON(sourceMapPath);
 
-validateSourceMapFormat(sourceMap, sourceMapPath);
-validateSourceFiles(sourceMap, originalFolderPath);
-validateSourceMapMappings(sourceMap, originalFolderPath, generatedFilePath);
+  validateSourceMapFormat(sourceMap, sourceMapPath);
+  validateSourceFiles(sourceMap, originalFolderPath);
+  validateSourceMapMappings(sourceMap, originalFolderPath, generatedFilePath);
+}
+
+validateSourceMap();
