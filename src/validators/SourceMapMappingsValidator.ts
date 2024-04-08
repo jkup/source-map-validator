8456376f268b7552595355026eb460423beb85de
Asumu Takikawa <asumu@igalia.com>
2024-04-07 20:52:30 -0700

    Catch errors from the source-map library
    Currently errors raised by the helper library are thrown without being
    converted into a validator error, which means errors in mapping parsing
    will just throw. This catches them and adds them to the error list.

--- src/validators/SourceMapMappingsValidator.ts
+++ src/validators/SourceMapMappingsValidator.ts
@@ -11,27 +11,31 @@ export class SourceMapMappingsValidator extends Validator {
     const originalFiles = collectSourceFiles(sourceMap, originalFolderPath);
     const generatedFile = TestingFile.fromPathBasedOnFileExtension(generatedFilePath);
 
-    await SourceMapConsumer.with(sourceMap, null, (consumer) => {
-      consumer.eachMapping((mapping) => {
-        // Is this a valid situation following the spec?
-        if (mapping.source === null) return
-        const originalFile = originalFiles.get(mapping.source);
+    try {
+      await SourceMapConsumer.with(sourceMap, null, (consumer) => {
+        consumer.eachMapping((mapping) => {
+          // Is this a valid situation following the spec?
+          if (mapping.source === null) return
+          const originalFile = originalFiles.get(mapping.source);
 
-        if (originalFile === undefined) {
-          errors.push(new Error(`There is no content for file with the path "${mapping.source}" on a disk or in the\`sourcesContent\` section`))
-          return
-        }
+          if (originalFile === undefined) {
+            errors.push(new Error(`There is no content for file with the path "${mapping.source}" on a disk or in the\`sourcesContent\` section`))
+            return
+          }
 
-        const notReasonableMappingMessage = this.formatWeirdMappingMessage(mapping, originalFile, generatedFile)
+          const notReasonableMappingMessage = this.formatWeirdMappingMessage(mapping, originalFile, generatedFile)
 
-        if (!originalFile.isMappingReasonable(mapping.originalLine, mapping.originalColumn)) {
-            errors.push(new Error(`${notReasonableMappingMessage} from the original file perspective`))
-        }
-        if (!generatedFile.isMappingReasonable(mapping.generatedLine, mapping.generatedColumn)) {
-          errors.push(new Error(`${notReasonableMappingMessage} from the generated file perspective`))
-        }
+          if (!originalFile.isMappingReasonable(mapping.originalLine, mapping.originalColumn)) {
+              errors.push(new Error(`${notReasonableMappingMessage} from the original file perspective`))
+          }
+          if (!generatedFile.isMappingReasonable(mapping.generatedLine, mapping.generatedColumn)) {
+            errors.push(new Error(`${notReasonableMappingMessage} from the generated file perspective`))
+          }
+        });
       });
-    });
+    } catch (exn : any) {
+      errors.push(new Error(exn.message));
+    }
 
     return ValidationResult.from(errors);
   }
